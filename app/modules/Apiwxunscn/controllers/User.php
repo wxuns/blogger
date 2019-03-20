00202127c518995e13ad68b2b7902c502437ee95
wxuns <php7ml@gmail.com>
2019-03-20 22:45:03 +0800

    sigup

--- app/modules/Apiwxunscn/controllers/User.php
+++ app/modules/Apiwxunscn/controllers/User.php
@@ -1,20 +1,23 @@
 <?php
 
-class UserController extends BaseController 
+class UserController extends BaseController
 {
     public function signupAction()
     {
         $data = Request($this->getRequest());
         if (!$data){
-            echo 'user message is null';
+            echo toJson(['errorcode'=>4000,'errormsg'=>'user message is null']);
         }else{
             $user = new UserModel();
             $msg = $user->where([
                 ['username','=',$data['username']],
                 ['password','=',md5($data['password'])]
             ])->first();
-            if ($msg)
-            echo toJson($msg);
+            if (!$msg){
+                echo toJson(['errorcode'=>4000,'errormsg'=>'user not find']);
+            }else{
+                echo toJson($msg);
+            }
         }
         return false;
     }
